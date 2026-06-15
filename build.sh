3517644cfdc50ecdaad51dc88da2d0d72a802f11
Guiihosvaldino <guilhermeosvaldino@hotmail.com>
2026-06-15 18:40:35 -0300

    Força criação de diretórios estáticos no build

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
+# Instala as dependências
 pip install -r requirements.txt
 
+# Força a criação das pastas para evitar que o Django dê erro de diretório inexistente
+mkdir -p staticfiles
+mkdir -p glicemia/static
+
+# Coleta os arquivos estáticos
 python manage.py collectstatic --no-input
+
+# Roda as migrações do banco de dados
 python manage.py migrate
