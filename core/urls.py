785f53e01c2ea2eac65a0ac537148deb466505db
Guiihosvaldino <guilhermeosvaldino@hotmail.com>
2026-06-15 17:58:58 -0300

    Adiciona rotas estaticas para o funcionamento do CSS em producao

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # <-- Certifique-se de que tem o 's' no final aqui!
+    path('admin/', admin.site.urls),
     path('', include('glicemia.urls')),
 ]
+
+# ESTAS LINHAS SÃO CRUCIAIS PARA O RECONHECIMENTO DO CSS EM PRODUÇÃO:
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
