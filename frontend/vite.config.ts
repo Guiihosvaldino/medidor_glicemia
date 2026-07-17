5229e12a85d4f08e30e9c4a9c4a0b0cf6b46d19a
Guiihosvaldino <guilhermeosvaldino@hotmail.com>
2026-07-17 16:19:34 -0300

    fix: ajustando caminhos de build relativos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🌟 ISSO AQUI força o Vite a usar caminhos relativos e resolve o erro no Netlify!
-  build: {
-    minify: 'terser', 
-    cssCodeSplit: false,
-  }
+  base: './'
 })
