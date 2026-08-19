bda56663b18b5eef018d47e93bfc124b82808438
Guiihosvaldino <guilhermeosvaldino@hotmail.com>
2026-08-19 00:49:06 -0300

    edicao

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,3 +11,5 @@ urlpatterns = [
     path('', include('glicemia.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
