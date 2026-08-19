2c20a59212e1bcd5479ea5154e0f76c9d057b6b6
Guiihosvaldino <guilhermeosvaldino@hotmail.com>
2026-08-19 19:14:56 -0300

    corrige local do logo e rota de estáticos

--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,15 @@ from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('glicemia.api_urls')),
     path('', include('glicemia.urls')),
+    
+    # Servimento de arquivos de mídia e estáticos no Railway
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
